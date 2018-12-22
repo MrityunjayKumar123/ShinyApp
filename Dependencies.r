d5e56fe19047e52b6879666fe7ec8cedf9b79c39
Mrityunjay Kumar <43465754+MrityunjayKumar123@users.noreply.github.com>
2018-12-22 17:09:07 +0530

    Add files via upload

---
+++ Dependencies.r
@@ -0,0 +1,24 @@
+
+
+if (!require(shiny)){install.packages("shiny")}
+if (!require(udpipe)){install.packages("udpipe")}
+if (!require(stringr)){install.packages("stringr")}
+if (!require(lattice)){install.packages("lattice")}
+if (!require(igraph)){install.packages("igraph")}
+if (!require(ggraph)){install.packages("ggraph")}
+if (!require(readtext)){install.packages("readtext")}
+if (!require(textrank)){install.packages("textrank")}
+if (!require(wordcloud)){install.packages("wordcloud")}
+
+
+
+library(shiny)
+library(udpipe)
+library(textrank)
+library(lattice)
+library(igraph)
+library(ggraph)
+library(ggplot2)
+library(wordcloud)
+library(stringr)
+
